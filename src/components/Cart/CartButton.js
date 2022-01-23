70d18cbcc798e7016c15e53caf311b8a0dbf9dc3
Veselin Hr. Trayanov <1.1@gmx.com>
2022-01-23 14:48:24 +0000

    ++ redux

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,12 +1,18 @@
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+
+     const toggleCartHandler = () => {
+
+     }
+     return (
+          <button className={classes.button}
+               onClick={toggleCartHandler}
+          >
+               <span>My Cart</span>
+               <span className={classes.badge}>1</span>
+          </button>
+     );
 };
 
 export default CartButton;
