56cd3990290434c5b0cb294d37204f8f2f7afc14
Veselin Hr. Trayanov <1.1@gmx.com>
2022-01-24 20:30:01 +0000

    Using an Action Creator Thunk

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/ui-slice'
-import Notification from './components/UI/Notification'
-
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+import { sentCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -13,60 +13,55 @@ let isInitial = true;
 function App() {
 
      const dispatch = useDispatch();
-
-     const showCart = useSelector(state => state.ui.cartIsVisible)
-
+     const showCart = useSelector(state => state.ui.cartIsVisible);
      const cart = useSelector(state => state.cart);
      const notification = useSelector(state => state.ui.notification);
-
-
-
-
 
      useEffect(() => {
 
           if (isInitial) {
-
+               
                isInitial = false;
                return;
 
-
           } else {
-               const sendCartData = async () => {
-                    dispatch(uiActions.showNotification({
-                         status: 'pending',
-                         title: 'Sending...',
-                         message: 'Sending cart data.',
-                    }));
-                    const response = await fetch('https://redux-side-effects-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-                         {
-                              method: 'PUT',
-                              body: JSON.stringify(cart)
-                         });
-                    if (response.ok) {
-                         dispatch(uiActions.showNotification({
-                              status: 'success',
-                              title: 'Success...',
-                              message: 'Sent cart data Successfully.',
-                         }));
-                    }
-               }
 
-               sendCartData().catch(error => {
-                    dispatch(uiActions.showNotification({
-                         status: 'error',
-                         title: 'Error...!',
-                         message: `Sending cart data Failed. (${error})`,
-                    }));
-               })
+               dispatch(sentCartData(cart));
 
+               //      const sendCartData = async () => {
+               // dispatch(uiActions.showNotification({
+               //      status: 'pending',
+               //      title: 'Sending...',
+               //      message: 'Sending cart data.',
+               // }));
+               // const response = await fetch('https://redux-side-effects-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+               //      {
+               //           method: 'PUT',
+               //           body: JSON.stringify(cart)
+               //      });
+               // if (response.ok) {
+               //      dispatch(uiActions.showNotification({
+               //           status: 'success',
+               //           title: 'Success...',
+               //           message: 'Sent cart data Successfully.',
+               //      }));
+               // }
+               // }
+
+               // sendCartData().catch(error => {
+               //      dispatch(uiActions.showNotification({
+               //           status: 'error',
+               //           title: 'Error...!',
+               //           message: `Sending cart data Failed. (${error})`,
+               //      }));
+               // })
           }
      }, [cart]);
 
 
      return (
           <>
-               {notification && (
+               { notification && (
                     <Notification
                          status={notification.status}
                          title={notification.title}
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
      name: 'cart',
@@ -54,6 +54,49 @@ const cartSlice = createSlice({
           },
      }
 });
+
+
+
+export const sentCartData = (cart) => {
+
+     return async (dispatch) => {
+
+          dispatch(uiActions.showNotification({
+               status: 'pending',
+               title: 'Sending...',
+               message: 'Sending cart data.',
+          }));
+
+          const sendCartData = async () => {
+
+               const response = await fetch('https://redux-side-effects-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+                    method: 'PUT',
+                    body: JSON.stringify(cart)
+               });
+
+               if (response.ok) {
+                    dispatch(uiActions.showNotification({
+                         status: 'success',
+                         title: 'Success...',
+                         message: 'Sent cart data Successfully.',
+                    }));
+               }
+          };
+
+          try {
+               await sendCartData();
+
+          } catch (error) {
+               dispatch(uiActions.showNotification({
+                    status: 'error',
+                    title: 'ERROR',
+                    message: `Sending DATA failed. ( ${error} )`,
+               }))
+          }
+     }
+};
+
+
 
 export const cartAction = cartSlice.actions;
 
