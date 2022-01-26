fe44bbe64ecde36dc0559e8f709a46f096a8ef07
Veselin Hr. Trayanov <1.1@gmx.com>
2022-01-26 13:06:40 +0000

    es lint

--- src/App.js
+++ src/App.js
@@ -10,28 +10,48 @@ import Notification from './components/UI/Notification';
 let isInitial = true;
 
 function App() {
+<<<<<<< HEAD
 
      const dispatch = useDispatch();
      const showCart = useSelector(state => state.ui.cartIsVisible);
      const cart = useSelector(state => state.cart);
      const notification = useSelector(state => state.ui.notification);
 
+=======
+     //new comment VS Code
+     const dispatch = useDispatch();
+     const showCart = useSelector((state) => state.ui.cartIsVisible);
+     const cart = useSelector((state) => state.cart);
+     const notification = useSelector((state) => state.ui.notification);
+
+     // console.log('cart', cart)
+>>>>>>> parent of 9baa9b3 (thanks SAND)
      useEffect(() => {
           dispatch(fetchCartData());
      }, [dispatch]);
 
      useEffect(() => {
+<<<<<<< HEAD
           if ( !isInitial) {
+=======
+          if (!isInitial) {
+>>>>>>> parent of 9baa9b3 (thanks SAND)
                dispatch(sentCartData(cart));
           } else {
                isInitial = false;
           }
      }, [cart, dispatch]);
 
+<<<<<<< HEAD
 
      return (
           <Fragment>
                {notification && notification.status !== 'done' && (
+=======
+     return (
+          <Fragment>
+               {notification && notification.status !== "done" && (
+>>>>>>> parent of 9baa9b3 (thanks SAND)
                     <Notification
                          status={notification.status}
                          title={notification.title}
