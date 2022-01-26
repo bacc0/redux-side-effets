3f86b15dea30f842dab52621bb6b3a8e615ba4ea
Veselin Hr. Trayanov <1.1@gmx.com>
2022-01-26 12:07:29 +0000

    TEST VS

--- src/App.js
+++ src/App.js
@@ -10,40 +10,40 @@ import Notification from "./components/UI/Notification";
 let isInitial = true;
 
 function App() {
-  //new comment
-  const dispatch = useDispatch();
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
-  const cart = useSelector((state) => state.cart);
-  const notification = useSelector((state) => state.ui.notification);
+     //new comment VS Code
+     const dispatch = useDispatch();
+     const showCart = useSelector((state) => state.ui.cartIsVisible);
+     const cart = useSelector((state) => state.cart);
+     const notification = useSelector((state) => state.ui.notification);
 
-  // console.log('cart', cart)
-  useEffect(() => {
-    dispatch(fetchCartData());
-  }, [dispatch]);
+     // console.log('cart', cart)
+     useEffect(() => {
+          dispatch(fetchCartData());
+     }, [dispatch]);
 
-  useEffect(() => {
-    if (!isInitial) {
-      dispatch(sentCartData(cart));
-    } else {
-      isInitial = false;
-    }
-  }, [cart, dispatch]);
+     useEffect(() => {
+          if (!isInitial) {
+               dispatch(sentCartData(cart));
+          } else {
+               isInitial = false;
+          }
+     }, [cart, dispatch]);
 
-  return (
-    <Fragment>
-      {notification && notification.status !== "done" && (
-        <Notification
-          status={notification.status}
-          title={notification.title}
-          message={notification.message}
-        />
-      )}
-      <Layout>
-        {showCart && <Cart />}
-        <Products />
-      </Layout>
-    </Fragment>
-  );
+     return (
+          <Fragment>
+               {notification && notification.status !== "done" && (
+                    <Notification
+                         status={notification.status}
+                         title={notification.title}
+                         message={notification.message}
+                    />
+               )}
+               <Layout>
+                    {showCart && <Cart />}
+                    <Products />
+               </Layout>
+          </Fragment>
+     );
 }
 
 export default App;
